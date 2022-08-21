65ae713ab0390f44e2062b80ab5928a1490adb65
Łukasz Langa <lukasz@langa.pl>
2022-08-21 23:37:30 +0200

    Allow collecting spectrographs from multiple channels at once

--- spectro.py
+++ spectro.py
@@ -6,41 +6,43 @@ spectro - creates a spectrogram suited for synchronized Full HD display.
 
 Usage:
     spectro [--window=WIN] [--step=STEP] [--brightness=BRI] [--fps=FPS] \
-[--width=WIDTH] [--height=HEIGHT] [--colors=COLORS] <file>
+[--width=WIDTH] [--height=HEIGHT] [--colors=COLORS] [--channels=CHANNELS] <file>
     spectro [--colors=COLORS] --show-palette
     spectro --help
 
 Options:
-    <file>             Path to a file.
-    --window=WIN       How big should a single chunk given to the FFT be. By
-                       default this is computed as 'sample_rate
-                       * bytes_per_sample' in the given audio file. Larger
-                       values give more detailed images but require more
-                       computation. The value should be a positive integer.
-    --step=STEP        How much should the window move between two consecutive
-                       FFT calculations. If this value is much smaller than
-                       `--window`, the resulting image is going to be wider but
-                       the overlap in calculations will result in a ripple
-                       effect. The value should be a positive integer. By
-                       default this is calculated automatically based on
-                       `--window` and `--fps`.
-    --brightness=BRI   Brightness computed by this program is linear, from
-                       black (when the FFT is 0) to white (when the FFT is at
-                       the absolute maximum within the file). This gives quite
-                       dark images which is why an input multiplier is used to
-                       bump brightness.  [default: 8]
-    --fps=FPS          Calculate step towards this amount of frames per second
-                       in a video. [default: 30]
-    --width=WIDTH      Prepend this amount of pixels left of the image.
-                       [default: 1920]
-    --height=HEIGHT    Crop higher frequencies to leave this many pixels.
-                       [default: 1080]
-    --colors=COLORS    Comma-separated HTML colors that will be introduced in
-                       the spectrum. The shortened form (e.g. #fff) is not
-                       supported.
-                       [default: #000000,#0000d0,#00a0a0,#00d000,#ffffff]
-    --show-palette     Don't generate a spectrogram but a palette instead.
-    -h, --help         This info.
+    <file>               Path to a file.
+    --window=WIN         How big should a single chunk given to the FFT be. By
+                         default this is computed as 'sample_rate
+                         * bytes_per_sample' in the given audio file. Larger
+                         values give more detailed images but require more
+                         computation. The value should be a positive integer.
+    --step=STEP          How much should the window move between two consecutive
+                         FFT calculations. If this value is much smaller than
+                         `--window`, the resulting image is going to be wider but
+                         the overlap in calculations will result in a ripple
+                         effect. The value should be a positive integer. By
+                         default this is calculated automatically based on
+                         `--window` and `--fps`.
+    --brightness=BRI     Brightness computed by this program is linear, from
+                         black (when the FFT is 0) to white (when the FFT is at
+                         the absolute maximum within the file). This gives quite
+                         dark images which is why an input multiplier is used to
+                         bump brightness.  [default: 8]
+    --fps=FPS            Calculate step towards this amount of frames per second
+                         in a video. [default: 30]
+    --width=WIDTH        Prepend this amount of pixels left of the image.
+                         [default: 1920]
+    --height=HEIGHT      Crop higher frequencies to leave this many pixels.
+                         [default: 1080]
+    --colors=COLORS      Comma-separated HTML colors that will be introduced in
+                         the spectrum. The shortened form (e.g. #fff) is not
+                         supported.
+                         [default: #000000,#0000d0,#00a0a0,#00d000,#ffffff]
+    --channels=CHANNELS  Which channels to consider in input, like: 0,1,2,3,4,5.
+                         [default: ALL]
+    --show-palette       Don't generate a spectrogram but a palette instead.
+    -h, --help           This info.
 """
 
 from __future__ import absolute_import
@@ -69,32 +71,41 @@ def kaiser(length):
     return numpy.kaiser(length, BETA)
 
 
-def bytes_from_pcm(pcm, window, step):
+def bytes_from_pcm(pcm, window, step, channels):
     """Yields byte chunks of `window` size from the `pcm` stream.
 
     Data is padded on both ends with half a window. This way both the first and
     the last FFT is computed against the first and the last sample in the audio
     file.
     """
+    first_ch = channels[0]
     _padding = b'\x00' * window
-    data = _padding
+    data = {ch: _padding for ch in channels}
     frames = pcm.read(CHUNK_SIZE)
     while len(frames):
-        data += frames.channel(0).to_bytes(False, True)
-        while len(data) >= window:
-            yield data[:window]
-            data = data[step:]
+        for ch in channels:
+            data[ch] += frames.channel(ch).to_bytes(False, True)
+        while len(data[first_ch]) >= window:
+            yield [data[ch][:window] for ch in channels]
+            for ch in channels:
+                data[ch] = data[ch][step:]
         frames = pcm.read(CHUNK_SIZE)
-    data += _padding
-    while len(data) >= window:
-        yield data[:window]
-        data = data[step:]
+    for ch in channels:
+        data[ch] += _padding
+    while len(data[first_ch]) >= window:
+        yield [data[ch][:window] for ch in channels]
+        for ch in channels:
+            data[ch] = data[ch][step:]
 
 
-def freq_from_pcm(pcm, window, step):
+def freq_from_pcm(pcm, window, step, channels):
     """Yields real FFTs from data chunks of `window` size in `pcm` stream."""
-    for chunk in bytes_from_pcm(pcm, window, step):
-        data = numpy.fromstring(chunk, 'int16')
+
+    for chunks in bytes_from_pcm(pcm, window, step, channels):
+        data = numpy.zeros(window // 2, numpy.int64)
+        for chunk in chunks:
+            data += numpy.frombuffer(chunk, 'int16')
+        data //= len(channels)
         yield numpy.fft.rfft(data * kaiser(len(data)))
 
 
@@ -122,6 +133,13 @@ def convert_html_to_hsv(colors):
         newcolors[-1] = last_color[0], white[1], white[2]
 
     return newcolors
+
+
+def convert_channels_to_list(channels):
+    if channels is None or channels == "ALL":
+        return []
+    
+    return [int(elem.strip()) for elem in channels.split(",")]
 
 
 def colors_to_buckets(colors, min=0, max=1):
@@ -178,7 +196,7 @@ def show_palette(colors):
 
 def main(
     file, window=None, step=None, brightness=8, prepend=0, fps=30,
-    crop_height=1080, colors=None,
+    crop_height=1080, colors=None, channels=None
 ):
     minimum = None
     maximum = None
@@ -188,9 +206,12 @@ def main(
 
     if not colors:
         colors = '#000000,#0000ff,#008080,#00ff00,#ffffff'
+    if not channels:
+        channels = 'ALL'
 
     colors = convert_html_to_hsv(colors)
     color_buckets = colors_to_buckets(colors, min=0, max=1)
+    channels = convert_channels_to_list(channels)
 
     audiofile = audiotools.open(file)
     freq_samples = []
@@ -199,6 +220,7 @@ def main(
         _samples_per_second = pcm.sample_rate * _bytes_per_sample
         window = window or _samples_per_second
         step = step or int(round(_samples_per_second / fps))
+        channels = channels or list(range(pcm.channels))
 
         print(file)
         print('     duration:', float(audiofile.seconds_length()))
@@ -207,9 +229,9 @@ def main(
         print('     channels:', pcm.channels)
         print('       window:', window)
         print('         step:', step, '(fps: {})'.format(fps))
-        print('calculating FFT...', end='\r')
+        print('Calculating FFT...', end='\r')
 
-        for freq in freq_from_pcm(pcm, window, step):
+        for freq in freq_from_pcm(pcm, window, step, channels):
             half = 0
             for modifier in [16, 12, 8, 4, 2]:
                 half = int(len(freq) / modifier)
@@ -244,7 +266,7 @@ def main(
     print(' image height:', height)
     print('    threshold:', threshold)
     print('      average:', average)
-    print('applying colors')
+    print('Applying colors...', end='\r')
 
     custom_black = get_color(0, brightness, color_buckets)
     for x in range(prepend):
@@ -257,11 +279,12 @@ def main(
             f /= threshold
             rgb[y, x] = get_color(f, brightness, color_buckets)
 
-    print('creating in-memory PNG image')
+    print('Creating in-memory PNG image', end='\r')
     i = Image.fromarray(rgb, mode='RGB')
     img_path = os.path.basename(file) + '.png'
-    print('saving image to', img_path)
+    print('Saving image to', img_path, ' ' * 10, end='\r')
     i.save(img_path)
+    print('Saved image to', img_path, ' ' * 10)
 
 
 if __name__ == '__main__':
@@ -299,4 +322,5 @@ if __name__ == '__main__':
             crop_height=args['--height'],
             fps=args['--fps'],
             colors=args['--colors'],
+            channels=args['--channels'],
         )
